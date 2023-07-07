d2518916aa22a9cf1cc41954c3a7028411df0bc4
sekun <sekunayo@gmail.com>
2023-07-07 10:19:19 -0400

    init

---
+++ RoomBookingApplication/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace RoomBookingApplication;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ RoomBookingApplication/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace RoomBookingApplication;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
