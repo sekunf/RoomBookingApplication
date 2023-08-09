1460ba9c2471f6880adccc447424a90d3a1ab327
sekun <sekunayo@gmail.com>
2023-08-09 18:10:18 -0400

    Rooms init

---
+++ RoomBookingApplication/BusinessLogic/Rooms.cs
@@ -0,0 +1,77 @@
+﻿using System;
+namespace RoomBookingApplication.BusinessLogic
+{
+	public class Rooms
+	{
+
+        
+
+        public enum Campus
+        {
+            DAV,
+            HMC,
+            TRA
+        }
+
+        public enum Type
+        {
+            GroupStudy,
+            IndividualStudy,
+            SilentSTudy,
+            
+        }
+            
+        public class Room
+        {
+            private string _roomName;
+            private int _roomNumber = 101;
+
+            public string RoomName
+            {
+                get
+                {
+                    return _roomName;
+                }
+                set
+                {
+                    _roomName = $"{Campus} {RoomNumber}";
+                }
+            }
+
+            public int RoomNumber
+            {
+                get
+                {
+                    return _roomNumber;
+                }
+
+                set
+                {
+                    _roomNumber++;
+                }
+
+            }
+
+            public int SeatingCapacity
+            {
+                get;
+                set;
+            }
+
+            public Campus Campus
+            {
+                get;
+                set;
+            }
+
+            public Type RoomType { get; set; }
+
+
+
+        }
+    }
+
+
+}
+
+
