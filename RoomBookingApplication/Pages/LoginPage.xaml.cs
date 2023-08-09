68508c33436d4b7f611f821980efdbfa7be58f28
nicolemorenas <morenas@shernet.sheridancollege.ca>
2023-08-09 16:57:44 -0400

    Login page initilizec (deleted maui program page by accident( BUT FIXED IT!))

---
+++ RoomBookingApplication/Pages/LoginPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace RoomBookingApplication.Pages;
+
+public partial class LoginPage : ContentPage
+{
+	public LoginPage()
+	{
+		InitializeComponent();
+	}
+
+    void LoginButton_Clicked(System.Object sender, System.EventArgs e)
+    {
+    }
+}
