16fb14530dfb2027c469f1e28b69aad8e42eb36b
sekun <130427513+sekunf@users.noreply.github.com>
2023-08-10 16:41:24 -0400

    merge

--- RoomBookingApplication/Pages/MyBookingsPage.xaml.cs
+++ RoomBookingApplication/Pages/MyBookingsPage.xaml.cs
@@ -1,9 +1,34 @@
-﻿namespace RoomBookingApplication.Pages;
+﻿using static Android.Provider.ContactsContract.CommonDataKinds;
+using RoomBookingApplication.BusinessLogic;
+namespace RoomBookingApplication.Pages;
 
 public partial class MyBookingsPage : ContentPage
 {
-	public MyBookingsPage()
-	{
-		InitializeComponent();
-	}
+	
+		
+
+        BookingManager _BookingManager = new BookingManager();
+
+
+        public BookingManager BookingManager
+        {
+            get
+            {
+                return _BookingManager;
+            }
+
+        }
+
+
+
+        public MyBookingsPage()
+        {
+        InitializeComponent();
+            
+
+            BindingContext = this;
+        }
+
+        
 }
+
